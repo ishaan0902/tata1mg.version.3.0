9e567679f586fa9bc8e3839a45cb0b9dddd420fc
ishaan0902 <83284952+ishaan0902@users.noreply.github.com>
2023-01-31 09:54:02 +0530

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -13,7 +13,7 @@ const Table = ({ title, data, j, tableHeadRow }) => {
       <div className="row">
         <span className="col column1">{tableHeadRow[j]} :</span>
         {isLink(data) ? (
-          <a href={data}>
+          <a href={data} target="-blank">
             <span className="col column2">Link</span>
           </a>
         ) : (
